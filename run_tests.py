878a20c8ee17a5a186b90f3d67b9495a13b1cf9f
Daniel Bauer <daniel.bauer@caserio.de>
2024-12-05 17:03:21 +0100

    Only test my djson parser

--- run_tests.py
+++ run_tests.py
@@ -520,6 +520,18 @@ programs = {
            "commands":[os.path.join(PARSERS_DIR, "test_nlohmann_json_20190718/bin/test_nlohmann_json")]
        }
 }
+programs = {
+   "C cJSON 1.7.3":
+       {
+           "url":"https://github.com/DaveGamble/cJSON",
+           "commands":[os.path.join(PARSERS_DIR, "test_cJSON_1_7_3/bin/test_cJSON")]
+       },
+    "Cstdj json":
+        {
+            "url": "https://dj-bauer.de",
+            "commands": [os.path.join(PARSERS_DIR, "../../../build/json")]
+        }
+}
 
 def run_tests(restrict_to_path=None, restrict_to_program=None):
 
